f8793dc4c4920fb25a4f39cd8f6dfde8714cf2d2
santzforever <107780102+santzforever@users.noreply.github.com>
2022-06-29 01:52:02 -0700

    Add files via upload

---
+++ coders day 1/index.js
@@ -0,0 +1,51 @@
+
+let country = 'nigeria'
+let city = 'lagos' 
+let surname = 'santz'
+console.log(`my name is ${name}, i am from ${country}, live in ${city}].`) 
+let string = 'santz of africa'
+let space = ' '
+let age = 100
+console.log(string.indexOf ('africa'))
+console.log(string.includes('santz'))
+console.log(country.includes('nig'))
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+let dancing = true
+let danceboy = false
+console.log (dancing==danceboy)
+let figure = [1, 2, 3]
+let numbers = figure
+console.log(figure==numbers)
+let fullname = name + space + surname;
+console.log(fullname);
+let fn = name + space + surname
+let mypersonalinfo = fullname +  '. I am ' + name + '. I live in '+ city + '. I am '+ age;
+console.log (mypersonalinfo)
+console.log( 'i hope everyone is enjoying. \nI hope you are ?')
+console.log('create\tmake\tbelieve')
+console.log('1\t2\t3')
+console.log('back slash (\\)' )
+console.log ('don\'t') 
+console.log (typeof 'Tolu')
+let challenge = '30DaysOfJavascript'
+console.log(string.indexOf('challenge'))
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge. substr(2,17))
+JSON.stringify(challenge)
+console.log(challenge. includes('script'))
+let industry = 'Apple,IBM,Oracle,Amazon,Microsoft,Facebook'
+console.log(industry.split(','))
+let str = '50 days of javascript' 
+console.log(str.replace('javascript', 'python'))
+console.log(str.charAt(15))
+let lastIndex = str.length -1
+console.log(str.charAt(lastIndex))
+console.log(str.charCodeAt('j'))
+console.log(str.lastIndexOf('a'))
+console.log(str.indexOf('a'))
+let word = 'you cannot end a sentence with because because because is a conjuction'
+console.log(word.indexOf('because'))
+console.log(word.lastIndexOf('because'))
+console.log(word.search('because'))
